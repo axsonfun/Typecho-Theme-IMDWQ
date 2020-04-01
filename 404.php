753fd2b0607604a13a65e540fb729832587a61a1
kicomodo <kicomodo@qq.com>
2020-04-01 11:53:57 +0800

    IMDWQ#3.0
    1.正式为木板增加暗色模式！
    2.优化获得PageSpeed双满分！
    3.删除Font Awesome并内置局部图标。
    4.CSS和JS文件采用延迟加载降低TTFB。
    5.判断浏览器是否处于暗色模式选择性加载。
    6.整合必要JS和CSS减少HTTP请求。
    7.删除音乐界面数字排序更改为图标。
    8.灯箱加载和上下一张动画统一风格。
    9.页面滚动背景图动画优化。
    10.文章页面增加日期显示格式。
    11.增加文章底部的版权显示条。
    12.语录界面出处增加小飞机图标。
    13.日记界面上下分割更紧凑。
    14.相册界面日期图标改为时钟。
    15.修复底部无法按下回到顶部的BUG。
    *因我删除搜索功能，故没有集成搜索图标，请自行添加。

--- 404.php
+++ 404.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <title>页面未找到 | <?php $this->options->biaoti(); ?></title>
 	<link rel="shortcut icon" href="<?php $this->options->icon(); ?>">
-    <link href="<?php $this->options->themeUrl('css.css'); ?>" rel="stylesheet" type="text/css"/>
     <meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1"/>
     <meta name="keywords" content="<?php $this->fields->guanjianci(); ?>"/>
     <meta name="description" content="<?php $this->fields->miaoshu(); ?>"/>
-    <link href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 <?php $this->need('sidebar.php'); ?>
